52f7a4f79fcf0ccc539c105f6611a8a2f3b16031
NMHH-ABA <NMHH.ABA@gmail.com>
2020-07-07 23:37:06 +0430

    Update youtube_dl_button.py

--- anydlbot/plugins/youtube_dl_button.py
+++ anydlbot/plugins/youtube_dl_button.py
@@ -485,6 +485,35 @@ async def youtube_dl_call_back(bot, update):
                         if metadata is not None:
                             if metadata.has("duration"):
                                 duration = metadata.get('duration').seconds
+                    # get the correct width, height, and duration for videos greater than 10MB
+                    if os.path.exists(thumb_image_path):
+                        width = 0
+                        height = 90
+
+                        # resize image
+                        # ref: https://t.me/PyrogramChat/44663
+                         #https://stackoverflow.com/a/21669827/4723940
+                        Image.open(thumb_image_path).convert(
+                            "RGB").save(thumb_image_path)
+                        img = Image.open(thumb_image_path)
+                        # https://stackoverflow.com/a/37631799/4723940
+                        #img.thumbnail((90, 90))
+                        if tg_send_type == "file":
+                            img.resize((320, height))
+                        else:
+                            img.resize((90, height))
+                        img.save(thumb_image_path, "JPEG")
+                        # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
+
+                        metadata = extractMetadata(createParser(thumb_image_path))
+                        if metadata.has("width"):
+                            width = metadata.get("width")
+                        if metadata.has("height"):
+                            height = metadata.get("height")
+                        if tg_send_type == "vm":
+                            height = width
+                    else:
+                        thumb_image_path = None
                     start_time = time.time()
                     # try to upload file
                     if tg_send_type == "audio":
